f46da3bf2cff13362bbc3f05c6c48350f4035f85
Max Spencer <max.spencer@guardian.co.uk>
2018-11-29 15:25:54 +0000

    Everything is Kotlin except TooLargeTool.java

---
+++ toolargetool/src/main/java/com/gu/toolargetool/ActivitySavedStateLogger.kt
@@ -0,0 +1,81 @@
+package com.gu.toolargetool
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+
+import java.util.HashMap
+
+/**
+ * [android.app.Application.ActivityLifecycleCallbacks] implementation that logs information
+ * about the saved state of Activities.
+ */
+class ActivitySavedStateLogger(
+        private val formatter: Formatter,
+        private val logger: Logger,
+        logFragments: Boolean
+) : Application.ActivityLifecycleCallbacks {
+
+    private val fragmentLogger = if (logFragments) FragmentSavedStateLogger(formatter, logger) else null
+    private val savedStates = HashMap<Activity, Bundle>()
+    var isLogging: Boolean = false
+        private set
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle) {
+        if (activity is FragmentActivity && fragmentLogger != null) {
+            activity.supportFragmentManager
+                    .registerFragmentLifecycleCallbacks(fragmentLogger, true)
+        }
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        if (activity is FragmentActivity && fragmentLogger != null) {
+            activity.supportFragmentManager
+                    .unregisterFragmentLifecycleCallbacks(fragmentLogger)
+        }
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        if (isLogging) {
+            savedStates[activity] = outState
+        }
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        val savedState = savedStates.remove(activity)
+        if (savedState != null) {
+            try {
+                val message = formatter.format(activity, savedState)
+                logger.log(message)
+            } catch (e: RuntimeException) {
+                logger.logException(e)
+            }
+
+        }
+    }
+
+    fun startLogging() {
+        isLogging = true
+        fragmentLogger?.startLogging()
+    }
+
+    fun stopLogging() {
+        isLogging = false
+        savedStates.clear()
+        fragmentLogger?.stopLogging()
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        // Unused
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        // Unused
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        // Unused
+    }
+}
---
+++ toolargetool/src/main/java/com/gu/toolargetool/FragmentSavedStateLogger.kt
@@ -0,0 +1,43 @@
+package com.gu.toolargetool
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+
+import java.util.HashMap
+
+/**
+ * [android.support.v4.app.FragmentManager.FragmentLifecycleCallbacks] implementation that
+ * logs information about the saved state of Fragments.
+ */
+class FragmentSavedStateLogger(private val formatter: Formatter, private val logger: Logger) : FragmentManager.FragmentLifecycleCallbacks() {
+    private val savedStates = HashMap<Fragment, Bundle?>()
+    private var isLogging = true
+
+    override fun onFragmentSaveInstanceState(fm: FragmentManager?, f: Fragment, outState: Bundle?) {
+        if (isLogging) {
+            savedStates[f] = outState
+        }
+    }
+
+    override fun onFragmentStopped(fm: FragmentManager, f: Fragment) {
+        val savedState = savedStates.remove(f)
+        if (savedState != null) {
+            try {
+                val message = formatter.format(fm, f, savedState)
+                logger.log(message)
+            } catch (e: RuntimeException) {
+                logger.logException(e)
+            }
+
+        }
+    }
+
+    internal fun startLogging() {
+        isLogging = true
+    }
+
+    internal fun stopLogging() {
+        isLogging = false
+    }
+}
